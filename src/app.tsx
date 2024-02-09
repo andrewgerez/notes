a467db5900197a721cbdc9d56a914d527d1f8488
Andrew Gerez <andrewgerez18@gmail.com>
2024-02-09 01:03:28 -0300

    feat: initial (home page)

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,25 @@
+import NewNoteCard from "./components/new-note-card";
+import NoteCard from "./components/note-card";
+
 export function App() {
   return (
-    <div className="text-3xl font-bold underline">start</div>
+    <div className="mx-auto my-12 max-w-6xl space-y-6">
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
   )
 }
